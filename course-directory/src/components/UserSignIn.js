13bc5f02a0e6f0f0eeac180f5fc942ba504c435a
localseth <97685638+localseth@users.noreply.github.com>
2023-01-02 19:03:07 -0600

    try to make UserSignIn component better

--- course-directory/src/components/UserSignIn.js
+++ course-directory/src/components/UserSignIn.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 const UserSignIn = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState('');
+
+    const handleCancel = (e) => {
+        e.preventDefault();
+        navigate(location.state || '/')
+    }
 
     return (
         <main>
@@ -17,12 +25,18 @@ const UserSignIn = () => {
                         id="emailAddress"
                         name="emailAddress"
                         type="email"
-                        value=""
-                        onChange={handleChange}
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
                         />
                     <label htmlFor="password">Password</label>
-                    <input id="password" name="password" type="password" value="" />
-                    <button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <button className="button" type="submit">Sign In</button><button className="button button-secondary" onClick={handleCancel}>Cancel</button>
                 </form>
                 <p>Don't have a user account? Click here to <Link to="/signup">sign up</Link>!</p>
                 
