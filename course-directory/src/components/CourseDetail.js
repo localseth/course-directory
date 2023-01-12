9258b9f3b509514864a0c8b2f488b994485668d1
Seth M <97685638+localseth@users.noreply.github.com>
2023-01-11 19:17:57 -0600

    cleaned up CourseDetail code and fixed a bug in which navigating to a newly created course would not wait for the updated information before rendering

--- course-directory/src/components/CourseDetail.js
+++ course-directory/src/components/CourseDetail.js
@@ -7,20 +7,10 @@ import { CourseContext, UserContext } from './Context';
 
 
 const CourseDetail = (props) => {
-    // const [course, setCourse] = useState([]);
-    // const [isLoading, setIsLoading] = useState(true);
     const [errors, setErrors] = useState([]);
 
     const params = useParams();
     const navigate = useNavigate();
-    
-    // const fetchCourse = useCallback( async () => {
-    //     await axios.get(`http://localhost:5000/api/courses/${params.id}`).then(response => {
-    //         console.log('fetching course #' + params.id, params);
-    //         setCourse(response.data);
-    //         setIsLoading(false);
-    //     });
-    // }, [params.id]);
 
     const { course, actions, isLoading } = useContext(CourseContext);
     const { authenticatedUser } = useContext(UserContext);
@@ -31,16 +21,12 @@ const CourseDetail = (props) => {
             .then(res => {
                 if (res.length) {
                     setErrors(res);
+                    console.log(errors);
                 } else {
                     navigate('/')
                 }
             });
     }, [params.id] );
-
-    const splitString = (str) => {
-        const newArr = str.split('\n');
-        return newArr;
-    };
 
     useEffect( () => {
         fetchCourse(params.id);
@@ -57,7 +43,7 @@ const CourseDetail = (props) => {
                     <div className='wrap'>
                         {authenticatedUser && authenticatedUser.emailAddress === course?.owner?.emailAddress ?
                             <>
-                                <Link to={`/courses/${course.id}/update`} className='button'>Update Course</Link>
+                                <Link to={`/courses/${course?.id}/update`} className='button'>Update Course</Link>
                                 <a href="#" className='button' onClick={handleDelete}>Delete Course</a>
                             </>
                             :
@@ -72,18 +58,17 @@ const CourseDetail = (props) => {
                         <div className='main--flex'>
                             <div>
                                 <h3 className='course--detail--title'>Course</h3>
-                                <h4 className='course--name'>{course.title}</h4>
-                                <p>By {course.owner.firstName} {course.owner.lastName}</p>
-                                <ReactMarkdown>{course.description}</ReactMarkdown>
-                                {/* {splitString(course.description).map((desc, i) => <p key={i}>{desc}</p>)} */}
+                                <h4 className='course--name'>{course?.title}</h4>
+                                <p>By {course?.owner?.firstName} {course?.owner?.lastName}</p>
+                                <ReactMarkdown>{course?.description}</ReactMarkdown>
                             </div>
                             <div>
                                 <h3 className='course--detail--title'>Estimated Time</h3>
-                                {course.estimatedTime ? <p>{course.estimatedTime}</p> : ""}
+                                {course?.estimatedTime ? <p>{course?.estimatedTime}</p> : ""}
                                 <h3 className='course--detail--title'>Materials Needed</h3>
                                 <ul className='course--detail--list'>
-                                    {course.materialsNeeded ? <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown> : null }
-                                        {/* splitString(course.materialsNeeded.replaceAll('* ', '').replace(/\n$/,'')).map((item, i) => <li key={i}>{item}</li>) */}
+                                    {course?.materialsNeeded ? <ReactMarkdown>{course?.materialsNeeded}</ReactMarkdown> : null }
+                                        
                                         
                                 </ul>
                             </div>
@@ -96,3 +81,23 @@ const CourseDetail = (props) => {
 }
 
 export default CourseDetail;
+
+/*
+ honorable mentions
+
+the code below are solutions I am proud of coming up with before I added the ReactMarkdown package
+
+This function took in the blocks of text as I saw them formatted in the api and split them at line breaks
+    const splitString = (str) => {
+        const newArr = str.split('\n');
+        return newArr;
+    };
+
+    For the course description, I used the above function first then generated <p> tags by mapping over the array
+    {splitString(course?.description).map((desc, i) => <p key={i}>{desc}</p>)}
+
+    Then for the materials needed, I did the same thing, but also deleted any asterisks thus relying on the formatting of the <li>
+    I also replaced any line that ended with a linebreak with an empty string
+    splitString(course?.materialsNeeded.replaceAll('* ', '').replace(/\n$/,'')).map((item, i) => <li key={i}>{item}</li>)
+*/
+
