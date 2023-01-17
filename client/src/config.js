9f9e85418440a2d21e6cb7c4723a0c9e49d5ea2e
Seth M <97685638+localseth@users.noreply.github.com>
2023-01-17 15:56:44 -0600

    change api url for deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // holds the api base url so it can be easily changed later in case the address changes
 const url = {
-    apiBaseUrl: 'http://localhost:5000/api',
+    apiBaseUrl: 'backend-production-ddb8.up.railway.app/api',
   };
 
   export default url
