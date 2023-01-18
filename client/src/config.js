a96a1961f02aeb4bb9cb0f9f18d46c5f2cbd96b2
localseth <97685638+localseth@users.noreply.github.com>
2023-01-18 12:39:05 -0600

    modify api address to include "https://"

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // holds the api base url so it can be easily changed later in case the address changes
 const url = {
-    apiBaseUrl: 'backend-production-ddb8.up.railway.app/api',
+    apiBaseUrl: 'https://backend-production-ddb8.up.railway.app/api',
   };
 
   export default url
